2f3bf0faafae68c9fd98e4aaf87adc6b04b90c57
Felipe Caldeira <felipe.xitus@gmail.com>
2026-02-13 15:16:08 -0300

    feat: add: implement Evento model and corresponding tests

---
+++ src/models/evento.js
@@ -0,0 +1,30 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable camelcase */
+// import db from '../db/dbconfig.js';
+
+class Evento {
+  constructor({
+    id, nome, descricao, data, autor_id, created_at, updated_at,
+  }) {
+    this.id = null || id;
+    this.nome = nome;
+    this.descricao = descricao;
+    this.data = data;
+    this.autor_id = autor_id;
+    this.created_at = created_at || new Date().toISOString();
+    this.updated_at = updated_at || new Date().toISOString();
+  }
+
+  static async pegarEventos() {
+    return [{
+      id: 1,
+      nome: 'Evento de Teste',
+      descricao: 'Descrição do evento de teste',
+      data: '2024-12-31',
+      autor_id: 1,
+      created_at: '2022-10-01 07:00:00',
+      updated_at: '2022-10-01 07:00:00',
+    }];
+  }
+}
+export default Evento;
